9f68464d6a8318b903c9735713a22281e14dcda7
sss985 <117765598+sss985@users.noreply.github.com>
2022-12-20 03:34:51 +0100

    Ajoutez des fichiers projet.

---
+++ SimpleConverter/BoolToVisibilityConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace SimpleConverter
+{
+    public class BoolToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool isMouseOver;
+
+            bool.TryParse(value.ToString(), out isMouseOver);
+
+            if (isMouseOver)
+            {
+                return Visibility.Visible;
+            }
+            return Visibility.Hidden;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
